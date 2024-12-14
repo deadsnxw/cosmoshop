58f2820a84790631bc6dbe371ba3e17b065f7e34
deadsnxw <maks.roskoshenko@gmail.com>
2024-12-14 16:37:28 +0200

    created project and added workflow

---
+++ src/main/java/org/example/cosmocats/CosmocatsApplication.java
@@ -0,0 +1,13 @@
+package org.example.cosmocats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmocatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CosmocatsApplication.class, args);
+    }
+
+}
